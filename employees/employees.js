a024657dd9e87db4ef38443031baa933c5303478
ayham-kattan <ayhamkattan1@gmail.com>
2022-01-06 01:58:37 +0200

    add-to-database

--- employees/employees.js
+++ employees/employees.js
@@ -1,63 +1,67 @@
 const express = require('express');
 const router = express.Router();
 const Joi=require('joi')
-const employees = [
-  { empID: 1, fullname: "John Doe", salary: 3000 },
-  { empID: 2, fullname: "depo Doe", salary: 4000 },
-  { empID: 3, fullname: "Jack Doe", salary: 5000 },
-  { empID: 4, fullname: "Jill Doe", salary: 6000 }
-];
+const mongoose = require('mongoose');
+const Employee = mongoose.model(
+  "Employee",
+  new mongoose.Schema({
+    fullname: { type: String, required: true, minlength: 3 },
+    salary: { type: Number, required: true, max: 100000 }
+  })
+);
 
-router.get("/", (req, res) => {
-
+router.get("/", async(req, res) => {
+  const employees=await Employee.find().sort('name');
   res.send(employees);
+  // res.send(employees);
 });
-router.get("/:id", (req, res) => {
-  const findEmployee = employees.find((emp) => emp.empID == req.params.id);
+router.get("/:id", async(req, res) => {
+  const findEmployee =await Employee.findById(req.params.id);
   if (!findEmployee) {
-    res.send("Employee not found");
+    res.status(404).send("Employee not found");
   }
   res.send(findEmployee);
 });
-router.post("/", (req, res) => {
+
+router.post("/", async(req, res) => {
   const { error } = emplyeeValidate(req.body);
   if (error) {
     return res.send(error.details[0].message);
   }
-  const newEmployee = {
-    empID: req.body.empID,
+  const employee =new Employee ({
+ 
     fullname: req.body.fullname,
     salary: req.body.salary,
-  };
-  employees.push(newEmployee);
-  res.send(employees);
+  });
+await employee.save()
+  res.send(employee);
 });
 
-router.put("/:id", (req, res) => {
-  const findEmployee = employees.find((emp) => emp.empID == req.params.id);
-  if (!findEmployee) {
-    res.send("Employee not found");
-  }
+router.put("/:id", async(req, res) => {
   const { error } = emplyeePutValidate(req.body);
   if (error) {
     return res.send(error.details[0].message);
   }
-  findEmployee.fullname = req.body.fullname;
-  res.send(findEmployee);
+  const employee = await Employee.findByIdAndUpdate(req.params.id, {
+    fullname: req.body.fullname,
+    salary: req.body.salary,
+  }, { new: true });
+  if (!employee) {
+    return res.status(404).send("invaled id");
+  }
+  res.send(employee);
 });
 
-router.delete("/:id", (req, res) => {
-  const findEmployee = employees.find((emp) => emp.empID == req.params.id);
+router.delete("/:id", async(req, res) => {
+  const findEmployee = await Employee.findByIdAndRemove(req.params.id);
   if (!findEmployee) {
-    res.send("Employee not found");
+    res.status(404).send("Employee not found");
   }
-  const employeeIndex = employees.indexOf(findEmployee);
-  employees.splice(employeeIndex, 1);
   res.send(findEmployee);
 });
 function emplyeeValidate(employee) {
   const schema = Joi.object({
-    empID: Joi.number().integer().max(100).required(),
+   
     fullname: Joi.string().min(3).required(),
     salary: Joi.number().integer().max(100000).required(),
   });
